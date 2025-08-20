5f7c297ca51168dffb70e97c211a950d768b3579
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-08-21 00:28:15 +0300

    created page templates. wrote pages: login, layout. added components: loading, sidebar, menuitems. added assets and user test no back yet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+// from ' https://clerk.com/ '
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// clerk.com - create form login page
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // from ' https://clerk.com/ '
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 )
