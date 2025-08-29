f212784aa59b0b10f5a044561bfa02f525693efb
Павло Кучерявих <pasha.kycher89@gmail.com>
2025-08-29 06:47:04 +0300

    changed pages: Connection, Feed, Messages. changed components: PostCard. created components: RecendMessages.

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
+import RecendMessages from '../components/RecendMessages'
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([])
@@ -49,13 +50,18 @@ const Feed = () => {
       </div>
 
       {/* Right sidebar  */}
-      <div className='hidden xl:block w-80 space-y-6'>
+      <div className='max-xl:hidden sticky top-0'>
 
-        <div>
-          <h1>Sponsores</h1>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='yexy-slate-800 font-semibold'>Sponsores</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>
+            Supercharge your marketing with a powerful, easy-to-use platform bult for results.
+          </p>
         </div>
 
-        <h1>Recend messages</h1>
+        <RecendMessages />
       </div>
     </div>
   ) : (
